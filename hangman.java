f5907d7541837e36a763999bc9706e43345151c1
brut3f0rc3 <palash.s.chatterjee@gmail.com>
2014-11-24 10:07:49 +0530

    Moved from Applet to JApplet

--- hangman.java
+++ hangman.java
@@ -1,88 +1,34 @@
-//Allowed errors = 6
+/*
+	Simulation of Hangman using Swing class.
+	JAVA Lab Group Project
+	Members : Nisha Sinha, Md.Sajid, Ratan Jain, Palash Chatterjee
+*/
 
-import java.awt.*;
-import java.applet.*;
-import java.awt.event.*;
-//For Box Layout
-import javax.swing.*;
+	import java.awt.*;
+	import java.awt.event.*;
+	import javax.swing.*;
 
-public class hangman extends Applet implements ActionListener{
-		boolean letterUsed[] = new boolean[26];
-		TextField name;
-		Button submit;
-		Canvas stage;
-		Panel pane;
-		TextField guess;
-		Label state, title, l1, l2, error, result;
-		int errors=0;
-		int allowed=6;
-		Font bold = new Font("Arial", Font.BOLD, 30);
-		Font normal = new Font("Arial", Font.PLAIN, 15);
-		boolean correct=false;
+//Class to handle Hangman drawing
+	class stage extends JPanel{
 
-		String word = "WIKIPEDIA";
-		String filler="";
-		String newFiller="";
-		String lives="";
-	
+		static int allowed;
 
-		public void init(){
-			setLayout(new BorderLayout());
-			setBackground(Color.white);
-			setForeground(Color.black);
-
-			//create Objects
-			stage = new Canvas();
-			pane = new Panel();
-			guess = new TextField(1);
-
-			title = new Label("Hangman", Label.CENTER);
-			l1=new Label("Current Status : ", Label.CENTER);
-			l2= new Label("Please enter your guess : ", Label.CENTER);
-		
-			
-
-			for (int i=0; i<word.length(); i++)
-				filler+="_ ";
-			state = new Label(filler.trim(), Label.CENTER);
-
-			for (int i=0; i<allowed; i++)
-				lives+="\u2764";
-			error = new Label(lives,Label.RIGHT);
-			
-
-			state.setFont(bold);
-			submit = new Button("Submit");
-			submit.addActionListener(this);
-			
-			add(title, BorderLayout.PAGE_START);
-			add(stage, BorderLayout.LINE_START);
-			add(pane, BorderLayout.LINE_END);
-			pane.setPreferredSize(new Dimension(300, 200));
-			pane.setLayout(new BoxLayout(pane, BoxLayout.PAGE_AXIS));
+		public stage(){
+			allowed=6;
 
 		}
 
-		public void paint(Graphics g){
+		public void setAllowed(int x){
+			allowed=x;
+			//System.out.println("Allowed : "+allowed);
+			//new Throwable().getStackTrace();
+			this.repaint();
+		}
 
-			pane.add(error);
-			pane.add(l1);
-			pane.add(state);
-			pane.add(l2);
-			pane.add(guess);
-			pane.add(submit);
-			
-			guess.requestFocus();
+		@Override public void paintComponent(Graphics g) {
+			super.paintComponent(g);
+			//System.out.println("hanged : "+allowed);
 
-			if(allowed==0){
-				pane.remove(guess);
-				pane.remove(submit);
-				pane.remove(l2);
-				result = new Label("Sorry! You have lost. The word was : ");
-				pane.add(result);
-			}
-
-			this.setSize(new Dimension(600,400));
 			switch(6-allowed){
 						//Right Foot
 				case 6:	g.drawLine(175,230,220,270);
@@ -97,95 +43,293 @@ public class hangman extends Applet implements ActionListener{
 						//Face
 				case 1: g.drawOval(150,100, 50,50);
 						//Rope
-						g.drawLine(175,57,175,100);
+				g.drawLine(175,57,175,100);
 
 				default: //Woodwork
-						g.fillRect(60,50,7,300);
-						g.fillRect(67,50,125,7);
-						g.fillRect(30,350,67,7);
+				g.fillRect(60,50,7,300);
+				g.fillRect(67,50,125,7);
+				g.fillRect(30,350,67,7);
 
 						//Diagonal
-						g.drawLine(67,87,97,57);
-						g.drawLine(67,88,98,57);
-						g.drawLine(67,89,99,57);
-						g.drawLine(67,90,100,57);
-						
+				g.drawLine(67,87,97,57);
+				g.drawLine(67,88,98,57);
+				g.drawLine(67,89,99,57);
+				g.drawLine(67,90,100,57);
+
+			}
+		}
+		@Override public Dimension getPreferredSize(){
+			return new Dimension(300,300);
+		}
+	}
+
+
+//Driver class
+	public class hangman extends JApplet implements ActionListener{
+
+		stage stagePanel = new stage();
+		JPanel controlPanel = new JPanel();
+		Box verticalBox = Box.createVerticalBox();
+		JPanel banner = new JPanel();
+		JLabel name = new JLabel("Hang-Man", SwingConstants.CENTER);;
+		JTextField guess = new JTextField(1);
+
+		JLabel guessLabel = new JLabel("Please enter your guess...");	
+		JLabel current;
+		JLabel currentLabel = new JLabel("Current State");;
+		JLabel lives;
+		JLabel result = new JLabel("");
+		JButton submit = new JButton("Submit");
+		JButton resetGame = new JButton(new AbstractAction("New Game"){
+			@Override
+			public void actionPerformed(ActionEvent ae){
+				reset();
+			}
+		});
+
+
+		String currentStatus=null;
+		String lifeStatus=null;
+		String selectedWord;
+		String guessedLetter, newFiller;
+		String userName;
+		int allowed;
+		boolean correct;
+		boolean letterUsed[] = new boolean[26];
+
+		public void init(){	
+
+			System.out.println(" _   _    __    _  _  ___  ____  ____  ");
+			System.out.println("( )_( )  /__\\  ( \\( )/ __)( ___)(  _ \\ ");
+			System.out.println(" ) _ (  /(__)\\  )  (( (_-. )__)  )(_) )");
+			System.out.println("(_) (_)(__)(__)(_)\\_)\\___/(____)(____/ ");
+			System.out.println("\n");
+			System.out.println("++++++++++++++++++++++++++++++++++++++++++");
+			System.out.println("................L O A D I N G.............");
+
+				selectedWord = words.wordreturn();
+				allowed = 6;
+				lifeStatus="";
+				for (int i=0; i<allowed; i++)
+					lifeStatus+="\u2764";
+				currentStatus="";
+				for (int i=0; i<selectedWord.length(); i++)
+					currentStatus+="_ ";
+				lives = new JLabel(lifeStatus, SwingConstants.RIGHT);
+				lives.setFont(new Font("Serif", Font.PLAIN, 20));
+				current	= new JLabel(currentStatus, SwingConstants.CENTER);
+				current.setFont(new Font("Serif", Font.PLAIN, 30));
+				submit.addActionListener(this);
+
+				guess.addKeyListener(new KeyAdapter(){
+					public void keyPressed(KeyEvent e){
+						int key = e.getKeyCode();
+						if(key==KeyEvent.VK_ENTER){
+							acceptInput();
 						}
-	
-		}
-
-
-		public boolean validateInput(String args){
-
-			if(args.length()>1){
-				showStatus("Please enter a single character only.");
-				return false;
-			}
-			else if(args.equals("")){
-				showStatus("Please provide an input.");
-				return false;
-			}
-			else if(!Character.isLetter(args.charAt(0))){
-				showStatus("Please enter only letters.");
-				return false;
-			}
-			else if(letterUsed[(int)args.toLowerCase().charAt(0)-97]){
-				showStatus(Character.toString(args.toUpperCase().charAt(0))+" has already been used.");
-				return false;
-			}
-			else{
-				letterUsed[(int)args.toLowerCase().charAt(0)-97]=true;
-				return true;
-			}
-
-		}
-
-
-		public void actionPerformed(ActionEvent e){
-			
-			String guessedLetter = guess.getText();
-			System.out.println(guessedLetter);
-
-			//Clear the text field and set focus
-			guess.setText("");
-			guess.requestFocus();
-
-			//Set defaults
-			filler = state.getText();
-			System.out.println("Current status : "+filler);
-			showStatus(filler);
-			filler.replaceAll("\\s+","");
-			System.out.println("Current status after space removal : "+filler);
-			System.out.println("String length : "+filler.length());
-			newFiller="";
-			lives="";
-			correct=false;
-			
-			if(validateInput(guessedLetter)){
-				System.out.println((int)guessedLetter.toLowerCase().charAt(0));
-				for(int i=0; i<word.length(); i++){
-
-					if(Character.toString(guessedLetter.charAt(0)).equalsIgnoreCase(Character.toString(word.charAt(i)))){
-			
-						newFiller+=guessedLetter.toUpperCase()+" ";
-						correct=true;
 					}
-					else{
-						newFiller+=Character.toString(filler.charAt(i*2))+" ";
-					}
+				});
+
+			}
+
+			public void reset(){
+				selectedWord = words.wordreturn();
+
+				for(int i=0; i<26; i++)
+					letterUsed[i]=false;
+
+				allowed = 6;
+
+				lifeStatus="";
+				for (int i=0; i<allowed; i++)
+					lifeStatus+="\u2764";
+
+				lives.setText(lifeStatus);
+
+				currentStatus="";
+				for (int i=0; i<selectedWord.length(); i++)
+					currentStatus+="_ ";
+
+				current.setText(currentStatus);
+
+				stagePanel.setAllowed(allowed);
+				this.repaint();
+				guessLabel.setText("Please enter your guess...");
+				guess.setEnabled(true);
+				submit.setEnabled(true);
+				result.setVisible(false);
+				showStatus("The game has been restarted.");
+				System.out.println("The game has been restarted.");
+
+			}
+			public void paint(Graphics g){
+				super.paint(g);
+				stagePanel.repaint();
+
+		//Banner
+				banner.setBackground(Color.CYAN);
+				banner.add(name);
+
+		//Control Panel
+				controlPanel.setBackground(Color.WHITE);
+				controlPanel.setLayout(new BoxLayout(controlPanel, BoxLayout.PAGE_AXIS));
+				verticalBox.add(lives);
+				verticalBox.add(currentLabel);
+				verticalBox.add(current);
+				verticalBox.add(guessLabel);
+				guess.setMaximumSize(new Dimension(getSize().width,50));
+				guess.setAlignmentX(SwingConstants.CENTER);
+				verticalBox.add(guess);
+				verticalBox.add(submit);
+				verticalBox.add(resetGame);
+				verticalBox.add(result);
+				controlPanel.add(verticalBox);
+				controlPanel.setBorder(BorderFactory.createEmptyBorder(50, 10, 50, 10));
+
+
+				if(allowed==0){
+
+					guessLabel.setText(" ");
+					guess.setEnabled(false);
+					submit.setEnabled(false);
+					result.setText("The word was : "+selectedWord.toUpperCase()+"  !!!");
+					System.out.println("The word was : "+selectedWord.toUpperCase()+"  !!!");
+					result.setVisible(true);
 				}
 
+		//Applet basic settings and positioning
+				this.setLayout(new BorderLayout());
+				this.add(stagePanel, BorderLayout.WEST);	
+				this.add(controlPanel, BorderLayout.CENTER);
+				this.add(banner, BorderLayout.NORTH);
 
-				if(!correct)
-					allowed--;
-				for (int i=0; i<allowed; i++)
-					lives+="\u2764";
-				System.out.println("Allowed : "+allowed);
-				error.setText(lives);
-				state.setText(newFiller.trim());
-				showStatus(newFiller);
-				System.out.println("Current status : "+newFiller);
-				repaint();
+			//Clear the text field and set focus
+				guess.setText("");
+				guess.requestFocus();
+
+			}
+
+			public void acceptInput(){
+				guessedLetter = guess.getText();
+
+		//Clear the text field and set focus
+				guess.setText("");
+				guess.requestFocus();
+
+		//Reset strings
+				lifeStatus="";
+
+				showStatus(currentStatus);
+				currentStatus.replaceAll("\\s","");
+			//showStatus(currentStatus);
+
+				if(validateInput(guessedLetter)){
+
+
+					newFiller="";
+					correct=false;
+
+					System.out.println("You guessed : "+guessedLetter.toUpperCase().charAt(0));
+					for(int i=0; i<selectedWord.length(); i++){
+
+						if(Character.toString(guessedLetter.charAt(0)).equalsIgnoreCase(Character.toString(selectedWord.charAt(i)))){
+
+							showStatus(guessedLetter.toUpperCase()+" is correct.");
+							newFiller+=guessedLetter.toUpperCase()+" ";
+							correct=true;
+						}
+						else{
+							newFiller+=Character.toString(currentStatus.charAt(i*2))+" ";
+						}
+					}
+
+
+					if(!correct)
+					{
+						allowed--;
+						showStatus(guessedLetter.toUpperCase()+" is incorrect.");
+					}
+					if(allowed==0)
+						lifeStatus="\u2639";
+					else{
+						for (int i=0; i<allowed; i++)
+							lifeStatus+="\u2764";
+					}	
+					lives.setText(lifeStatus);
+					currentStatus = newFiller.trim();
+					current.setText(currentStatus);
+					System.out.println("Current status : "+newFiller);
+					stagePanel.setAllowed(allowed);
+
+					if(selectedWord.equalsIgnoreCase(current.getText().replaceAll("\\s","")))
+					{	
+						result.setText("Congratulations!");
+						System.out.println("Congratulations!");
+						result.setVisible(true);
+						guess.setEnabled(false);
+						submit.setEnabled(false);
+					}
+					
+			//stagePanel.repaint();
+					this.repaint();
+				}
+			}
+
+			public void actionPerformed(ActionEvent ae){
+
+				acceptInput();
+
+			}
+
+			public boolean validateInput(String args){
+
+				if(args.length()>1){
+					showStatus("Please enter a single character only.");
+					return false;
+				}
+				else if(args.equals("")){
+					showStatus("Please provide an input.");
+					return false;
+				}
+				else if(!Character.isLetter(args.charAt(0))){
+					showStatus("Please enter only letters.");
+					return false;
+				}
+				else if(letterUsed[(int)args.toLowerCase().charAt(0)-97]){
+					showStatus(Character.toString(args.toUpperCase().charAt(0))+" has already been used.");
+					return true;
+				}
+				else{
+					letterUsed[(int)args.toLowerCase().charAt(0)-97]=true;
+					return true;
+				}
+
 			}
 		}
-}
+
+//Class to generate words at random for the words input
+		class words
+		{
+			public static String wordreturn()
+			{
+				String wordarray[]={"Scrabble","crossword","maintain","second","adjacent","Diagonal","premium","Distribution","Quantity",
+				"Hangman","computer","appropriate","television","prompt","immediately","arrangement","possible","calculate",
+				"comproportionation","exuberant","flambouyant","LEXICOLOGY","ambidexterous","philanthropist","flabbergast","quiver"
+				,"tubectomy","impotent","important","buoyancy","bazinga","Parliament","zealous","yesteryear","xenophobia","workaholic",
+				"Swagger","oblivious","valedictorian","Undocumented","nincompoop","Stallion","zeitgeist","braille","binary",
+				"claustrophobic","encyclopedia","exaggeration","flamingos","freaks","google","graphical","horoscope","hysterical",
+				"judiciary","justification","Pandora","krushkal","kardashian","lullaby","lollipop","mythology","mystique",
+				"nonchalant","questionaire","Procrastination","Karaoke","Cryptography","Sublime", "Organic","Countermeasure","Ethical","University","Project","Hangman"};
+				int upper=wordarray.length-1;
+				int lower=0;
+				int r = (int) (Math.random() * (upper - lower)) + lower;
+				try{
+					return wordarray[r];
+				}
+				catch(Exception e){
+					r = (int) (Math.random() * (upper - lower)) + lower;
+					return wordarray[r];
+				}
+			}
+
+		}
